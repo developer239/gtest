8dffda609754c62c2a7b8f6cf14acf0d17440fb9
michaljarnot <m.jarnot@yahoo.com>
2022-05-08 01:20:35 +0200

    test: add mock example

---
+++ src/unit-6/Employee.h
@@ -0,0 +1,12 @@
+#ifndef GOOGLE_TEST_SAMPLE_EMPLOYEE_H
+#define GOOGLE_TEST_SAMPLE_EMPLOYEE_H
+
+#include <string>
+
+struct Employee {
+  int id;
+  float salary;
+  std::string name;
+};
+
+#endif
